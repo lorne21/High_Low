a33dcd876f702e270b2f9f9b3aa1a081bbce77b1
Lorne Barfield <lornebarfield1@Lornes-MacBook-Pro.local>
2015-07-13 14:16:18 -0500

    highlow

---
+++ highlow.php
@@ -0,0 +1,37 @@
+<?php
+
+function randomNumber(){ 
+
+	$rand = rand(1, 100); 
+	echo $rand . PHP_EOL;
+	$attempts = 0; 
+
+	fwrite(STDOUT, 'Guess a number between 1 and 100: ') . PHP_EOL;
+
+	$userGuess = fgets(STDIN);
+
+	do {
+		if ($userGuess > $rand){ 
+			$attempts += 1;
+			// echo ($attempts);
+			fwrite(STDOUT, 'Lower. Try again: ') . PHP_EOL; 
+			$userGuess = fgets(STDIN);   
+		} else if ($userGuess < $rand){
+			$attempts += 1;
+			// echo ($attempts);
+			fwrite(STDOUT, 'Higher. One more time: ') . PHP_EOL; 
+			$userGuess = fgets(STDIN);  
+		}
+	} while ($userGuess != $rand); 
+
+	if ($userGuess == $rand){
+		$attempts += 1; 
+		fwrite(STDOUT, 'This is why everyone hates you, Professor X.  ') . PHP_EOL; 
+		echo 'It took ' . $attempts . ' attempts'; 
+	} 
+}
+
+randomNumber();
+
+
+?>

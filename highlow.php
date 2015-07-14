c90999abfec2d9d1de92e6ba3e2d5c8cdeb128c9
Lorne Barfield <lornebarfield1@Lornes-MacBook-Pro.local>
2015-07-14 09:59:32 -0500

    highlow revision

--- highlow.php
+++ highlow.php
@@ -1,12 +1,22 @@
 <?php
 
-function randomNumber(){ 
+if ($argc == 1) {
+    fwrite(STDOUT, 'Please pick a minimum number: ') . PHP_EOL;
+	$min = fgets(STDIN);
+	fwrite(STDOUT, 'Please pick a maximum number: ') . PHP_EOL;
+	$max = fgets(STDIN);
+} else if ($argc == 3){
+	$min = $argv[1];
+	$max = $argv[2];
+}
 
-	$rand = rand(1, 100); 
+function randomNumber($min, $max){ 
+
+	$rand = rand($min, $max); 
 	echo $rand . PHP_EOL;
 	$attempts = 0; 
 
-	fwrite(STDOUT, 'Guess a number between 1 and 100: ') . PHP_EOL;
+	fwrite(STDOUT, 'What number am I thinking ') . PHP_EOL;
 	$userGuess = fgets(STDIN);
 
 	do {
@@ -24,7 +34,6 @@ function randomNumber(){
 	} while ($userGuess != $rand); 
 
 	if ($userGuess == $rand){
-		$y = 'y'; 
 		$attempts += 1; 
 		fwrite(STDOUT, 'This is why everyone hates you, Professor X.  ') . PHP_EOL; 
 		echo 'It took ' . $attempts . ' attempts.' . PHP_EOL; 
@@ -38,7 +47,8 @@ function randomNumber(){
 	} 
 }
 
-randomNumber();
+randomNumber($min, $max);
+
 
 
 ?>

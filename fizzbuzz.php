66bf462d55ae394e47619c6d1f4664d6c57ab870
Lorne Barfield <lornebarfield1@Lornes-MacBook-Pro.local>
2015-07-14 11:29:03 -0500

    fizzbuzz

---
+++ fizzbuzz.php
@@ -0,0 +1,22 @@
+<?php
+
+
+for ($test = 1; $test <= 100; $test++){
+	if ($test % 15 == 0){
+		echo "FizzBuzz" . PHP_EOL;
+	} else if ($test % 5 == 0){
+		echo "Buzz" . PHP_EOL;
+	} else if ($test % 3 == 0){
+		echo "Fizz" . PHP_EOL;
+	} else {
+		echo $test . PHP_EOL; 
+	}
+}
+
+
+
+
+
+
+
+?>
